efdbaa8d8dd9cfb9b46b388c7fb1cb806c21b8a4
Alamenium <youssifayman2004@gmail.com>
2022-11-22 20:25:48 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+
+const feelingWords = ['hate', 'despise', 'dislike', 'would rather die than', 'like', 'love', 'appreciate', 'would love']
+const verbs = fs.readFileSync('verbs.txt','utf-8').split("\n")
+const nouns = fs.readFileSync('nouns.txt', 'utf-8').split("\n")
+
+main();
+
+function main (){
+    const name = 'Youssif'
+
+    const f_index = RandomIndex(feelingWords);
+    const v_index = RandomIndex(verbs);
+    const n_index = RandomIndex(nouns);
+
+    const verb = AdjustVerb(verbs[v_index]);
+    const feelingWord = feelingWords[f_index];
+    const noun = nouns[n_index];
+
+    const ascii = `%C
+    ___    __   ________    ______          __     ______           _    
+   /   |  / /  <  /__  /   /_  __/__  _  __/ /_   / ____/__  ____  (_)__ 
+  / /| | / /   / / /_ <     / / / _ \\| |/_/ __/  / / __/ _ \\/ __ \\/ / _ \\
+ / ___ |/ /___/ /___/ /    / / /  __/>  </ /_   / /_/ /  __/ / / / /  __/
+/_/  |_/_____/_//____/    /_/  \\___/_/|_|\\__/   \\____/\\___/_/ /_/_/\\___/ 
+`
+
+    const phrase = `My name is ${name} and I ${feelingWord} ${verb} ${noun}`;
+
+    console.log(ascii)
+    console.log(phrase)
+}
+
+
+
+
+
+function RandomIndex(arr){
+    return Math.floor(Math.random() * (arr.length))
+}
+
+function AdjustVerb (verb){
+    if(verb[verb.length-1]==='e') {
+        verb = verb.slice(0, verb.length - 1);
+    }
+
+    return (verb+'ing')
+}
